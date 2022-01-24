c2d6aa9c6671fdb85b1579756a46c961653c7c36
ashraf <aelneima@aimsammi.org>
2022-01-24 20:53:28 +0200

    update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,18 +1,3 @@
-# YELLOW='\033[1;33m'
-# GREEN='\033[0;32m'
-# NC='\033[0m' # No Color
-
-# if [ "$1" = "" ]
-# then
-#     echo -e "\n${YELLOW}Warning: Not using GPU ${NC}\n"
-# else
-#     echo -e "\n${GREEN}Using CUDA device $1 ${NC}\n"
-# fi
-
-# export CUDA_VISIBLE_DEVICES=$1
-# export PYTHONPATH=$PWD:$PYTHONPATH
-
-
 YELLOW='\033[1;33m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
@@ -33,3 +18,17 @@ fi
 
 export CUDA_VISIBLE_DEVICES=$1,$2
 export PYTHONPATH=$PWD:$PYTHONPATH
+
+# YELLOW='\033[1;33m'
+# GREEN='\033[0;32m'
+# NC='\033[0m' # No Color
+
+# if [ "$1" = "" ]
+# then
+#     echo -e "\n${YELLOW}Warning: Not using GPU ${NC}\n"
+# else
+#     echo -e "\n${GREEN}Using CUDA device $1 ${NC}\n"
+# fi
+
+# export CUDA_VISIBLE_DEVICES=$1
+# export PYTHONPATH=$PWD:$PYTHONPATH
